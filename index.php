ae42d5500960bae54728f9e9b1c661e60a4bc106
Theo Martin <theo.martin22@icloud.com>
2017-09-26 21:39:19 +0200

    [+] FO : lancement

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <title>Théo Martin - Développeur Web</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-grid.min.css">
@@ -13,9 +13,57 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-<div class="container">
-    <h1>Hello, world!</h1>
-</div>
+<main class="container">
+    <p id="mt-hello">Hello, world!</p>
+    <h1>My name is, Théo Martin</h1>
+    <h2>I am a web developer</h2>
+    <ul>
+        <li>WordPress <3</li>
+        <li>Prestashop</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript / jQuery</li>
+        <li>PHP / Smarty</li>
+        <li>Git / Github / BitBucket</li>
+        <li>SCSS</li>
+        <li>Gulp</li>
+        <li>Grunt</li>
+        <li>Compass</li>
+    </ul>
+    <h3>Contact me and imagine the web together!</h3>
+
+    <p>theomartin.ste@gmail.com</p>
+
+    <!-- <div class="form-group row">
+        <label for="example-text-input" class="col-3 col-form-label">Your Name</label>
+        <div class="col-9">
+            <input class="form-control" type="text" value="Artisanal kale" id="example-text-input">
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="example-email-input" class="col-3 col-form-label">Your Email</label>
+        <div class="col-9">
+            <input class="form-control" type="email" value="bootstrap@example.com" id="example-email-input">
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="example-color-input" class="col-3 col-form-label">Your favorite color</label>
+        <div class="col-9">
+            <input class="form-control" type="color" value="#563d7c" id="example-color-input">
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="exampleTextarea" class="col-3">Your Message</label>
+        <div class="col-9">
+            <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+        </div>
+    </div> -->
+
+    <h4>Not Merge! REBASE!</h4>
+</main>
 
 <!-- jQuery first, then Tether, then Bootstrap JS. -->
 <script src="node_modules/jquery/dist/jquery.min.js"></script>
